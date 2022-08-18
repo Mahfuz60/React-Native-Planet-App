c1db583ed28d17b4ac62ae5a18b3b46343c25c69
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-08-19 03:31:56 +0600

    project setup

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme/colors';
+import { spaceing } from './src/theme/spaceing';
+import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+  if (!loaded) {
+    return <Text>Font is Loading....</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontFamily: typography.bold, fontSize: spaceing[8] }}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      <Text style={{ marginTop: spaceing[4] }}>Hello Planet App</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,7 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/theme/spaceing.js
@@ -0,0 +1,1 @@
+export const spaceing = [2, 4, 8, 12, 16, 20, 24, 28, 32];
